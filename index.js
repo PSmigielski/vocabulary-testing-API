d5fec2a785eeccc8d1514d01c29d94c77e0ea889
psmigielski <walek.smigielski@gmail.com>
2020-11-07 17:40:37 +0100

    add env.example, change readme and db connect

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
+const db = require('./database/index')
 const express = require('express');
 const Routes = require('./routes/index');
-require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-
-
 app.use('/v1/api/words', Routes);
 
 const port = process.env.PORT || 3000;
-
+db.connect(function(err) {
+    if (err) throw err;
+    console.log('DATABASE CONNECTED');
+});
 app.listen(port, () => console.log(`Server started on port ${port}`));
