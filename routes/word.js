770179cda6cbaaa73d0065bb1a2611c5688505cf
psmigielski <walek.smigielski@gmail.com>
2021-01-04 16:10:42 +0100

    add checkJwt middleware to word routes

--- routes/word.js
+++ routes/word.js
@@ -1,9 +1,10 @@
-const router =  require('express').Router();
-const wordController = require('../controllers/word.controller');
+const router = require("express").Router();
+const wordController = require("../controllers/word.controller");
+const checkJwt = require("../middleware/checkJwt");
 
-router.post('/create', wordController.create);
-router.post('/upload', wordController.upload);
-router.get('/show', wordController.show);
-router.delete('/delete', wordController.delete);
+router.post("/create", checkJwt, wordController.create);
+router.post("/upload", checkJwt, wordController.upload);
+router.get("/show", checkJwt, wordController.show);
+router.delete("/delete", checkJwt, wordController.delete);
 
 module.exports = router;
